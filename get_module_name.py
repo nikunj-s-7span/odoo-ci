0e8f474720e07a9635bfe04368a4a3449aee9d65
Nikunj S <nikunj.s@7span.com>
2025-04-15 16:44:18 +0530

    [IMP]crm_customization: updated script file to fix CI issues

--- get_module_name.py
+++ get_module_name.py
@@ -3,12 +3,10 @@ import subprocess
 
 # Get the commit message from the GITHUB_EVENT_PATH environment variable
 commit_message = subprocess.check_output(['git', 'log', '-1', '--pretty=%B']).decode().strip()
-
-print(f"Commit message: {commit_message}")
-
+# print(f"Commit message: {commit_message}")
 # Use regular expressions to extract the module name from the commit message
 match = re.search(r'\[[^\]]+\]([a-zA-Z0-9_]+):', commit_message)
 
 if match:
-    print(f"Module name: {match.group(1)}")
+    # print(f"Module name: {match.group(1)}")
     print(match.group(1))
